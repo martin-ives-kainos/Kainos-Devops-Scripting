6d135e789fda58eba7a000ad44ac0aff8de5fd3c
martin.ives <martin.ives@kainos.com>
2026-08-20 11:25:43 +0100

    Enhance SetupLocalEnv.ps1 to create default settings and verify PowerShell 7 installation; add Find-PwshInstallation function for executable detection

--- PowerShell/SetupLocalEnv.ps1
+++ PowerShell/SetupLocalEnv.ps1
@@ -1,10 +1,39 @@
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $false)]
-    [string[]]$reqdModules = @("PSIni"),
-    [bool]$FixModulePath = $false
+#    [Parameter(Mandatory = $false)]
+#    [string[]]$reqdModules = @("PSIni"),
+#    [bool]$FixModulePath = $false
 )
 $ErrorActionPreference = "Stop"
+
+Import-Module -Name (Join-Path $PsScriptRoot "UtilsModule" -Resolve) -ErrorAction Stop
+
+$defaultSettingsFile = ($PSCommandPath -replace "\.ps1$", ".settings.json")
+
+if (-not (Test-Path $defaultSettingsFile -PathType Leaf)) {
+    Write-Host "Default settings file not found: creating $defaultSettingsFile"
+    $defaultSettings = @{
+        "reqdModules"      = @(
+            "PSIni"
+            "Pester"
+        )
+        "LocalAppFolder"    = 'utils'
+        "FixModulePath"    = $FixModulePath
+        "LocalModulePaths" = @(
+            "Documents\WindowsPowerShell\Modules"
+            "Documents\PowerShell\Modules"
+        )
+    }
+    $defaultSettings | ConvertTo-Json -Depth 99 | Out-File -Append $defaultSettingsFile
+}
+$defaultSettings = Get-Content -Path $defaultSettingsFile -Raw | ConvertFrom-Json
+$userSettingsFile = Join-Path $env:APPDATA ((Split-Path $PSCommandPath -Leaf) -replace "\.ps1$", ".user.settings.json")
+
+if (-not (Test-Path $userSettingsFile -PathType Leaf)) {
+    Write-Host "User settings file not found: creating $userSettingsFile"
+    $defaultSettings | ConvertTo-Json -Depth 99 | Out-File -Append $userSettingsFile
+}
+$userSettings = Get-Content -Path $userSettingsFile -Raw | ConvertFrom-Json
 
 $utilsAppPath = Join-Path $HOME "utils"
 
@@ -13,29 +42,44 @@ if (-not (Test-Path $utilsAppPath -PathType Container)) {
     New-Item -ItemType Directory -Path $utilsAppPath -Force | Out-Null
 }
 
+$pwshPath = Split-Path (Find-PwshInstallation) -Parent
+if (-not $pwshPath) {
+    Write-Host "PowerShell 7 executable not found. Please install PowerShell 7 and ensure it is in your PATH."
+    exit 1
+}
+
+$unblockLog = Join-Path $pwshPath "unblock.log"
+if (-not (Test-Path $unblockLog -PathType Leaf)) {
+    Write-Host "Unblocking PowerShell 7 executable: $pwshPath"
+    Get-ChildItem $pwshPath -Recurse | Unblock-File
+    New-Item -ItemType File -Path $unblockLog -Force -Value "Unblocked on $(Get-Date)" | Out-Null
+}
+
+# Ensure that the PowerShell 7 directory is in the user PATH environment variable
+Update-UserEnvPath -VariableName "Path" -NewPath $pwshPath
+
 $PsVersion = $PSVersionTable.PSVersion
 if ($PsVersion.Major -lt 7) {
     Write-Host "PowerShell version 7 or higher is required. Current version: $PsVersion"
 
+    # We already found the pwsh executable, so we can add its directory to the user PATH if it's not already there.
+    # $pwshExecutable = Get-Command pwsh -ErrorAction SilentlyContinue | Sort-Object Version -Descending | Select-Object -First 1
     # Check for PowerShell 7 installation
-    $pwshExecutable = Get-ChildItem -Path $utilsAppPath -Filter "pwsh.exe" -Recurse -ErrorAction SilentlyContinue | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+    # $pwshExecutable = Get-ChildItem -Path $utilsAppPath -Filter "pwsh.exe" -Recurse -ErrorAction SilentlyContinue | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 
-    if (-not $pwshExecutable) {
-        Write-Host "PowerShell 7 not found in utils directory: $utilsAppPath"
-        exit 1
-    }
 
+    <#  TODO: See the utility function Update-UserEnvPath in UtilsModule/public/Update-UserEnvPath.ps1 for a more robust implementation of updating the user environment variable.
     $allPaths = [environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::User).Trim(';') -split ';'
     if ($allPaths -notcontains $pwshExecutable.DirectoryName) {
         Write-Host "Adding PowerShell 7 directory to PATH: $($pwshExecutable.DirectoryName)"
         $newPath = $pwshExecutable.DirectoryName + ";" + [environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::User)
         [Environment]::SetEnvironmentVariable("PATH", $newPath, [System.EnvironmentVariableTarget]::User)
-    }
+    } #>
 }
 
 Import-Module -Name (Join-Path $PsScriptRoot "UtilsModule") -ErrorAction Stop
 
-Function Fix-PSModulePath {
+function Fix-PSModulePath {
 
     $psModPath = [Environment]::GetEnvironmentVariable("PSModulePath", [System.EnvironmentVariableTarget]::User) -split ';'
 
@@ -85,6 +129,17 @@ if (-not $psGallery.Trusted) {
 
 $psModPath = [Environment]::GetEnvironmentVariable("PSModulePath", [System.EnvironmentVariableTarget]::User) -split ';'
 
-Install-Module -Name PsIni -Scope CurrentUser -Force -AllowClobber
+foreach ($module in $userSettings.reqdModules) {
+    if (-not (Get-Module -ListAvailable -Name $module)) {
+        Write-Host "Installing required module: $module"
+        Install-Module -Name $module -Scope CurrentUser -Force -AllowClobber
+
+    }
+    else {
+        Write-Host "Required module already installed: $module"
+    }
+}
+
+<# Install-Module -Name PsIni -Scope CurrentUser -Force -AllowClobber
 $installedModules = Get-InstalledModule -ErrorAction SilentlyContinue
-$installedModules
+$installedModules #>
--- PowerShell/UtilsModule/public/Update-UserEnvPath.ps1
+++ PowerShell/UtilsModule/public/Update-UserEnvPath.ps1
@@ -8,12 +8,29 @@ function Update-UserEnvPath {
         [Parameter(Mandatory = $true)]
         [string]$NewPath
     )
-    $currentValue = ([Environment]::GetEnvironmentVariable($VariableName, [System.EnvironmentVariableTarget]::User) -split ';') -ne '' ? ([Environment]::GetEnvironmentVariable($VariableName, [System.EnvironmentVariableTarget]::User) -split ';') : @()
+    # TODO: Implement logic to update the user environment variable with the new path, ensuring no duplicates and maintaining proper formatting.
+
+    $currPaths = @{
+        'System' = [Environment]::GetEnvironmentVariable($VariableName, [System.EnvironmentVariableTarget]::System) -split ';'
+        'User' = [Environment]::GetEnvironmentVariable($VariableName, [System.EnvironmentVariableTarget]::User) -split ';'
+    }
+
+<# 
+    $allPaths = [environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::User).Trim(';') -split ';'
+    if ($allPaths -notcontains $pwshExecutable.DirectoryName) {
+        Write-Host "Adding PowerShell 7 directory to PATH: $($pwshExecutable.DirectoryName)"
+        $newPath = $pwshExecutable.DirectoryName + ";" + [environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::User)
+        [Environment]::SetEnvironmentVariable("PATH", $newPath, [System.EnvironmentVariableTarget]::User)
+    }
+#>
+
+
+    <#     $currentValue = ([Environment]::GetEnvironmentVariable($VariableName, [System.EnvironmentVariableTarget]::User) -split ';') -ne '' ? ([Environment]::GetEnvironmentVariable($VariableName, [System.EnvironmentVariableTarget]::User) -split ';') : @()
     if ($currentValue -notcontains $NewPath) {
         $updatedValue = $currentValue + $NewPath
         [Environment]::SetEnvironmentVariable($VariableName, ($updatedValue -join ';'), [System.EnvironmentVariableTarget]::User)
         Write-Host "Updated user environment variable '$VariableName' with new path: $NewPath"
     } else {
         Write-Host "The path '$NewPath' is already present in the user environment variable '$VariableName'. No update needed."
-    }
+    } #>
 }
